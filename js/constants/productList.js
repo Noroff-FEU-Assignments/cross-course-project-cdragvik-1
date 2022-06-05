7ec00af8545bb3130260d3b9c559f318aba89ef8
cdragvik <christiane.dragvik@stud.noroff.no>
2022-06-05 20:14:18 +0200

    summary

---
+++ js/constants/productList.js
@@ -0,0 +1,39 @@
+const productArray - [
+    {
+        name: "Tolga jacket", 
+        img: src="images/Tolga jacket.jpg", 
+        id: "1", 
+        price: 300,
+        
+    }, 
+    {
+        name: "Drevsjo jacket", 
+        img: src="images/Drevsjo jakcet.jpg", 
+        id: "1", 
+        price: 250,
+    }, 
+    {
+        name: "Geiranger jacket", 
+        img: src="images/Geiranger jacket.jpg", 
+        id: "1", 
+        price: 400,
+    }, 
+    {
+        name: "Smola jacket", 
+        img: src="images/Smola_jacket.jpg", 
+        id: "1", 
+        price: 800,
+    }, 
+    {
+        name: "Hvaler jacket", 
+        img: src="images/Hvaler_jacket.jpg", 
+        id: "1", 
+        price: 550,
+    }, 
+    {
+        name: "Hesseng jacket", 
+        img: src="images/Tolga jacket.jpg", 
+        id: "1", 
+        price: 620,
+    }
+]
