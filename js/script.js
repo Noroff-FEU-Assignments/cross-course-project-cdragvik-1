78ab915de02b0b7810c2b180524d5c8395eb266d
cdragvik <cndragvik@gmail.com>
2022-08-19 14:15:26 +0200

    Summery
    Used JS to fetch data from WordPress REST API and added it to products page.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const url = "http://rainy-days.local/wp-json/wc/store/products";
+const productContainer = document.querySelector(".our_jackets");
+
+async function getProducts(){
+    try{
+        const response = await fetch(url);
+        const getResults = await response.json();
+        createHTML(getResults);
+    }
+
+    catch(error){
+        console.log(error);
+    }
+}
+
+getProducts();
+
+
+function createHTML(products){
+    products.forEach(function(product){
+        productContainer.innerHTML += `<div class="product_list"> 
+        <img src="${product.images[0].src}" alt="${product.name}" class="jacket_img">
+        <h3>${product.name}</h3> 
+        <p>${product.price} kr </p>
+        </div>`;
+    })
+}
